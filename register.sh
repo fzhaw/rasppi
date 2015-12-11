38ac32618152e01b45292bbd32197e87c7b52f29
pi <pi@zhaw.ch>
2015-12-11 16:52:45 +0000

    added register script

---
+++ register.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ip=`curl http://169.254.169.254/latest/meta-data/public-ipv4 --connect-timeout 1 2>/dev/null`
+if [ -z "$ip" ]; then
+ ip=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'  | head -c -1`
+fi
+name='screen101'
+display=':1'
+#echo "registering ip $ip ..."
+curl -k -v -X POST -H 'Content-Type: application/json' -d '{"notify_url":"https://'$ip':1880/notify","name":"'$name'", "display":"'$display'", "sharing_url":"https://'$ip':8080/sharing"}' https://bettercast.ddns.net:8080/register
